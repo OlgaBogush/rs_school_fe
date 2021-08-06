2e3133dec8836f5b88391e327b32904f0708c16b
Olga Bogush <bohush.olga@gmail.com>
2021-08-06 10:57:13 +0300

    fix: added buttons to the video section

---
+++ museum/index.js
@@ -0,0 +1,7 @@
+const progress = document.querySelector('.progress');
+  
+progress.addEventListener('input', function() {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #82CFD0 0%, #82CFD0 ${value}%, #fff ${value}%, white 100%)`
+})
+ 
